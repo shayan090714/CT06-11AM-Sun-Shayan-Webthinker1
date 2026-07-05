a543c51473b277d6d0f841b6228e24bb41323e37
shayan090714 <kamini_s29@yahoo.com>
2026-07-05 04:14:27 +0000

    Jul 5, 2026, 4:14 AM

--- L01_redcircle.js
+++ L01_redcircle.js
@@ -7,6 +7,7 @@ function draw(){
     fill(255, 0, 0); //255,0,0
     noStroke(); //no outline
     ellipse(300,400,50,50);
+    fill(255, 0, 0); //255,0,0
     
     // Todo: Challenge 3 Traffic Light
 }
