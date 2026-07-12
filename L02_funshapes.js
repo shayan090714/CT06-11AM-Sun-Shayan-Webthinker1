d3500e1fcdf35c7e73660181f41a99c78768b0a8
shayan090714 <kamini_s29@yahoo.com>
2026-07-12 04:12:51 +0000

    Jul 12, 2026, 4:12 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -10,7 +10,7 @@ function draw() {
   ellipse(100,100,80,50);
   ellipse(200,100,80,50);
   fill("white");
-  circle(25,100,40,25)
+  circle(75,100,40,25)
   circle(125,100,40,25)
   fill("black")
   
