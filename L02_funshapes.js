232df1e5f9c08faf3f94b4c13c3a9def7cd4a74d
shayan090714 <kamini_s29@yahoo.com>
2026-07-12 04:09:51 +0000

    Jul 12, 2026, 4:09 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -8,7 +8,7 @@ function setup(){
 
 function draw() {
   ellipse(100,100,80,50);
-  ellipse(100,100,80,50);
+  ellipse(200,100,80,50);
   fill("gold");
   
   // Challenge 1: An eye using ellipse() function
