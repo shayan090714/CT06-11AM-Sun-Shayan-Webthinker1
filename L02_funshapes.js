52fb02565a80c36db24e078ae7503edac20d70f6
shayan090714 <kamini_s29@yahoo.com>
2026-07-12 04:17:14 +0000

    Jul 12, 2026, 4:17 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -13,7 +13,7 @@ function draw() {
   circle(100,100,40,25)
   circle(200,100,40,25)
   fill("white")
-  rect(50,100)
+  rectangle(50,100)
   fill("grey")
   // Challenge 1: An eye using ellipse() function
 
