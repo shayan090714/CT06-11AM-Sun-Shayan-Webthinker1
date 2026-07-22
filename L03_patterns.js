0b8689ad6431ec544546a23d225fcfee7073189c
shayan090714 <kamini_s29@yahoo.com>
2026-07-22 08:24:52 +0000

    Jul 22, 2026, 8:24 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -2,7 +2,7 @@ let colorValue = 0;
 function setup() {
     createCanvas(600,400);
     background(220);
-    noLoop(); // Stops continuous drawing
+    // noLoop(); // Stops continuous drawing
   }
   
 function draw() {
