69ac870757a986fc21b25b3300450ad364973602
shayan090714 <kamini_s29@yahoo.com>
2026-07-26 03:33:50 +0000

    Jul 26, 2026, 3:33 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -4,13 +4,13 @@ function setup(){
 }
 function draw(){
     let xpos = 550;
-    let ypos = 550;
+    let ypos = 50;
     let ggcolour = 0;
     for (let i = 0;i<5;i++){
         fill(0);
         circle(xpos,ypos,50);
         xpos = xpos - 50;
-        ypos = ypos - 50;
+        ypos = ypos + 50;
         ggcolour = ggcolour + 50
     }   
 }
