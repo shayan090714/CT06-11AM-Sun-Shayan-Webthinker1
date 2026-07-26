7b662df207ddd5acc8648c01d376ec79ae7bc22e
shayan090714 <kamini_s29@yahoo.com>
2026-07-26 03:57:45 +0000

    Jul 26, 2026, 3:57 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -18,7 +18,7 @@ function draw(){
     fill(shapeColor);
     circle(width/2,height/2,200);
     function mousePressed(){
-        shapeColor = (random());
+        shapeColor = (random(0,255));
     }
     function mouseReleased(){
         shapeColor = ("white");
