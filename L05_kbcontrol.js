6b722b7452ca44fabf41d9143dc615cd7bdf6d60
shayan090714 <kamini_s29@yahoo.com>
2026-08-02 03:20:59 +0000

    Aug 2, 2026, 3:20 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -9,3 +9,12 @@ function draw(){
     
 }
 
+function mousePressed(){
+        size = 5;
+        colour = color(random(0,255),random(0,255),random(0,255));
+    }
+    function mouseDragged(){
+    fill(colour);
+    circle(mouseX,mouseY,size);
+    size = size + 0.5;
+    }
