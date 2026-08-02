68ced2cff22e26c1fe23981f1bdf18b152f4fed6
shayan090714 <kamini_s29@yahoo.com>
2026-08-02 03:59:29 +0000

    Aug 2, 2026, 3:59 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -40,7 +40,7 @@ function draw(){
     }
     if (showRect){
         fill("red")
-        rect(width/2-size,height/2-size,size,size*2);
+        Rect(width/2-size,height/2-size,size,size*2);
     }
     if (showSquare){
         fill("blue")
