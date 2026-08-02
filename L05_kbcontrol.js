1abecfb334efcbfd45e635b344a54a676ce54408
shayan090714 <kamini_s29@yahoo.com>
2026-08-02 03:56:46 +0000

    Aug 2, 2026, 3:56 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -44,7 +44,7 @@ function draw(){
     }
     if (showSquare){
         fill("blue")
-        showSquare(width/2,height/2,size,size);
+        square(width/2,height/2,size,size);
     }
 }
 function keyPressed(){
