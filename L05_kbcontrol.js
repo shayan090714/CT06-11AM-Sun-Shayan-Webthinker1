7e006e98f84de3569de43278dfb182c85213e67d
shayan090714 <kamini_s29@yahoo.com>
2026-08-02 03:37:44 +0000

    Aug 2, 2026, 3:37 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,24 +1,24 @@
-let size = 5;
-let colour = "blue";
+// let size = 5;
+// let colour = "blue";
 
-function setup(){
-    createCanvas(600,400);
-    background('grey');
+// function setup(){
+//     createCanvas(600,400);
+//     background('grey');
     
-}
-function draw(){
+// }
+// function draw(){
     
-}
+// }
 
-function mousePressed(){
-        size = 5;
-        colour = color(random(0,255),random(0,255),random(0,255));
-    }
-    function mouseDragged(){
-    fill(colour);
-    circle(mouseX,mouseY,size);
-    size = size + 0.5;
-    }
+// function mousePressed(){
+//         size = 5;
+//         colour = color(random(0,255),random(0,255),random(0,255));
+//     }
+//     function mouseDragged(){
+//     fill(colour);
+//     circle(mouseX,mouseY,size);
+//     size = size + 0.5;
+//     }
 
 
 
