16c63e06ff3a9433f8d97baae9c27b2f1974f362
shayan090714 <kamini_s29@yahoo.com>
2026-08-23 03:59:51 +0000

    Aug 23, 2026, 3:59 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -23,11 +23,11 @@ if (ballX > width){
 } 
 if (ballX < 0){
     ballSpeedX = -1 * ballSpeedX
-    fill(random(0,255),0,0);
+    fill(random(0,255),0,random(0,255));
 } 
 if (ballY > height){
     ballSpeedY = -1 * ballSpeedY
-    fill(random(0,255),0,0);
+    fill(random(0,255),0,);
 } 
 if (ballY < 0){
     ballSpeedY = -1 * ballSpeedY
