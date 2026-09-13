6f799a6c8faa0fd0564d2225997a619292a82482
shayan090714 <kamini_s29@yahoo.com>
2026-09-13 03:47:36 +0000

    Sep 13, 2026, 3:47 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -31,3 +31,26 @@ if (ypos<0){
 }
 
 
+let list=["Pizza","Burger","Sushi","Tacos","Pasta"]
+function setup(){
+    createCanvas(600,400);
+}
+let yPos = 400;
+let lineGap = 60;
+function draw(){
+    background(0);
+    // textSize(20);
+    // text("I am Shayan",50,50);
+    
+    fill(255,255,0);
+    textSize(24);
+    textAlign(CENTER);
+    for (let i = 0;i<list.length;i++){
+        text((i+1)+"."+list[i],width/2,yPos+i*lineGap);
+    }
+    yPos -= 0.6;
+if(yPos< -list.length*lineGap){
+    yPos = height;
+}
+
+}
