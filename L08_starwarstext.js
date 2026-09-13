6ab1b770bdb1f8d4c3de6975b3ec4833c2e639ab
shayan090714 <kamini_s29@yahoo.com>
2026-09-13 03:25:48 +0000

    Sep 13, 2026, 3:25 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -11,6 +11,6 @@ function draw(){
     textAlign(LEFT);
     text("My Favourite Foods:")
     for (let i=0;i<FileList.length;i++)(
-        text((i+1)+"."+list[5])
+        text((i+1)+"."+list[i])
     )
 }
