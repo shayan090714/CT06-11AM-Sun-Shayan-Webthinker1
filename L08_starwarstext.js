9adc8f04e3172292bad5660ccce05e1ebf2d99af
shayan090714 <kamini_s29@yahoo.com>
2026-09-13 03:53:24 +0000

    Sep 13, 2026, 3:53 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -5,3 +5,25 @@ let storyText = [
     "Our adventure begins here",
     "Anything is possible",
 ]
+function setup(){
+    createCanvas(600,400);
+}
+let yPos = 400;
+let lineGap = 60;
+function draw(){
+    background(0);
+    // textSize(20);
+    // text("I am Shayan",50,50);
+    
+    fill(255,255,0);
+    textSize(24);
+    textAlign(CENTER);
+    for (let i = 0;i<list.length;i++){
+        text((i+1)+"."+list[i],width/2,yPos+i*lineGap);
+    }
+    yPos -= 0.6;
+if(yPos< -list.length*lineGap){
+    yPos = height;
+}
+
+}
