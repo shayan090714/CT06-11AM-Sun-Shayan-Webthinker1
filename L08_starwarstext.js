0596ebfb6804b0941210d6a0fcb6e973b4d44a06
shayan090714 <kamini_s29@yahoo.com>
2026-09-13 03:34:46 +0000

    Sep 13, 2026, 3:34 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -8,11 +8,8 @@ function draw(){
     // textSize(20);
     // text("I am Shayan",50,50);
     
-    textSize(20);
-    textAlign(LEFT);
-    text("My Favourite Foods:")
-    for (let i=0;i<list.length;i++){
-        text((i+1)+"."+list[i],50,i*30);
-    }
+    // 
+
+    
     yPos -= 0.6;
 }
