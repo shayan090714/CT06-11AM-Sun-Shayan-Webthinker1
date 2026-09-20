dc81fbfb644081d55422b078c926032855aa63f9
shayan090714 <kamini_s29@yahoo.com>
2026-09-20 04:12:35 +0000

    Sep 20, 2026, 4:12 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -4,7 +4,7 @@ let bgColour = "grey";
 let bgMusic;
 
 function preload(){
-    bgMusic = loadSound('')
+    bgMusic = loadSound("assets/bossanova.mp3")
 }
 function setup(){
     createCanvas(400, 400);
