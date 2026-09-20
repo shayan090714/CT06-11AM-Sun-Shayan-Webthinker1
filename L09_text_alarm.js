eea7448216eb604ce399e378c7ad71fae1794aba
shayan090714 <kamini_s29@yahoo.com>
2026-09-20 03:37:43 +0000

    Sep 20, 2026, 3:37 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -10,6 +10,6 @@ function draw(){
     let sec = second();
     fill(0);
     textSize(72);
-    text(nf(h,2)+":"+ nf(m,2)+":"+sec,width/2,height/2);
+    text(nf(h,2)+":"+ nf(m,2)+":"+nf(sec,2)+":"+sec,width/2,height/2) ;
     
 }
