c50a733245bb257beaea40e1ecfd1fa30fd60101
shayan090714 <kamini_s29@yahoo.com>
2026-09-20 03:12:54 +0000

    Sep 20, 2026, 3:12 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,1 +1,16 @@
-// write your codes here
+function setup(){
+    createCanvas(600, 400);
+    background('grey'); // 220
+}
+
+function draw(){
+    fill('red'); //255,0,0
+    noStroke(); //no outline
+    ellipse(300,100,50,50);
+    fill('orange');
+    ellipse(300,200,50,50);
+    fill('green'); //255,0,0
+    ellipse(300,300,50,50);
+    
+    // Todo: Challenge 3 Traffic Light
+}
